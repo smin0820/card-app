8c2abeaed5305dece5173e96e8d5e734a8dc6d3f
smin0820 <97029953+smin0820@users.noreply.github.com>
2025-04-22 16:10:36 +0900

    feat: Text, Button 공통 컴포넌트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,28 @@
-import { css } from '@emotion/react'
-import styled from '@emotion/styled'
-import React from 'react'
-import logo from './logo.svg'
 import './App.css'
-
-const containerStyles = css`
-  background-color: pink;
-`
-
-const Button = styled.button`
-  width: 200px;
-  height: 100px;
-`
+import Text from '@shared/Text'
+import Button from '@shared/Button'
 
 function App() {
   return (
-    <div className="App" css={containerStyles}>
-      <Button>스타일 버튼</Button>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Text typography="t1" display="block">
+        t1
+      </Text>
+      <Text typography="t2" color="red">
+        t2
+      </Text>
+      <Text typography="t3">t3</Text>
+      <Text typography="t4">t4</Text>
+      <Text>t5</Text>
+
+      <div style={{ height: 10, width: '100%', background: '#efefef' }}></div>
+
+      <Button>클릭해주세요</Button>
+      <Button color="success">클릭해주세요</Button>
+      <Button color="error">클릭해주세요</Button>
+      <Button color="error" weak={true}>
+        클릭해주세요
+      </Button>
     </div>
   )
 }
