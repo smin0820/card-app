0c911edc3bf2bad9e43a8276d697b6ef7e923e20
smin0820 <97029953+smin0820@users.noreply.github.com>
2025-04-22 17:59:16 +0900

    feat: Input, TextField 공통 컴포넌트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css'
 import Text from '@shared/Text'
 import Button from '@shared/Button'
+import Input from './components/shared/input'
+import TextField from './components/shared/TextField'
 
 function App() {
   return (
@@ -23,6 +25,12 @@ function App() {
       <Button color="error" weak={true}>
         클릭해주세요
       </Button>
+
+      <Input placeholder="로그인" aria-invalid={false} />
+      <Input aria-invalid={true} />
+
+      <TextField label="아이디" />
+      <TextField label="패스워드" />
     </div>
   )
 }
--- src/styles/colorPalette.ts
+++ src/styles/colorPalette.ts
@@ -7,7 +7,7 @@ export const colorPalette = css`
     --green: #4caf50;
     --white: #fff;
     --black: #212121;
-    --gray: #9e9e9e;
+    --grey: #9e9e9e;
   }
 `
 
@@ -17,7 +17,7 @@ export const colors = {
   green: 'var(--green)',
   white: 'var(--white)',
   black: 'var(--black)',
-  gray: 'var(--gray)',
+  grey: 'var(--grey)',
 }
 
 export type Colors = keyof typeof colors
