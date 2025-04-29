fc7f0ffeebbec066e1e69b4f9cbab634128612e0
smin0820 <97029953+smin0820@users.noreply.github.com>
2025-04-29 21:36:32 +0900

    feat: 회원가입 폼 상태 관리 및 입력 필드 연결

--- src/components/signup/Form.tsx
+++ src/components/signup/Form.tsx
@@ -3,17 +3,59 @@ import FixedBottomButton from '@shared/FixedBottomButton'
 import Flex from '@shared/Flex'
 import TextField from '@shared/TextField'
 import Spacing from '@shared/Spacing'
+import { ChangeEvent, useCallback, useState } from 'react'
+import { FormValues } from '@/models/signup'
 
 export default function Form() {
+  const [formValues, setFormValues] = useState<FormValues>({
+    email: '',
+    password: '',
+    rePassword: '',
+    name: '',
+  })
+
+  const handleFormValues = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setFormValues((prevFormValues) => ({
+      ...prevFormValues,
+      [e.target.name]: e.target.value,
+    }))
+  }, [])
+
+  console.log('formValues', formValues)
+
   return (
     <Flex direction="column" css={formContainerStyles}>
-      <TextField label="이메일" placeholder="card@gmail.com" />
+      <TextField
+        label="이메일"
+        name="email"
+        placeholder="card@gmail.com"
+        value={formValues.email}
+        onChange={handleFormValues}
+      />
       <Spacing size={16} />
-      <TextField label="패스워드" type="password" />
+      <TextField
+        label="패스워드"
+        name="password"
+        type="password"
+        value={formValues.password}
+        onChange={handleFormValues}
+      />
       <Spacing size={16} />
-      <TextField label="패스워드 재확인" type="password" />
+      <TextField
+        label="패스워드 재확인"
+        name="rePassword"
+        type="password"
+        value={formValues.rePassword}
+        onChange={handleFormValues}
+      />
       <Spacing size={16} />
-      <TextField label="이름" placeholder="홍길동" />
+      <TextField
+        label="이름"
+        name="name"
+        placeholder="홍길동"
+        value={formValues.name}
+        onChange={handleFormValues}
+      />
 
       <FixedBottomButton label="회원가입" onClick={() => {}} disabled={true} />
     </Flex>
